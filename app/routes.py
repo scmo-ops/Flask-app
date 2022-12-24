f1ca05b7a0cec729eabdc43fd493f897e6f7b2d0
Juan Francisco Camberos <juanfco.camberos@gmail.com>
2022-12-23 18:25:17 -0600

    Added posts to the site, I struggled really hard because I named my index indet sadge

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,16 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-def index():
-    user = {'username' : 'Miguel'}
-    return render_template('index.html', title = 'Home', user = user)
-    
+def index(): 
+    user = {'username' : 'Miguel'} # right now, since I don't have the usernames and their posts; I creted them to focus on rendering and building the blog first.
+    posts = [
+        {
+            'author':{'username':'Jhon'},
+            'body': 'I need to learn statisctics, advanced math, and advanced sql and python '
+        },
+        {
+            'author': {'username':'Susan'},
+            'body': 'Im Skyler white yo'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts=posts)
