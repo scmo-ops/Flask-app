2ecd335db48010fc8bc8fa75eccf10094cf64819
Juan Francisco Camberos <juanfco.camberos@gmail.com>
2023-01-25 13:29:45 -0600

    Added the log in page and rendered it

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from config import Config
 
 app = Flask(__name__)
-app.config.from_obejct(Config) # This line is telling flask to read and apply my config file.
+app.config.from_object(Config) # This line is telling flask to read and apply my config file.
 
 from app import routes
